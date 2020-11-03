bf68d734c068cac9617adf4b4070283766dfd6d0
Isaac <adetonaisaac12345@gmail.com>
2020-11-03 07:02:47 +0100

    search by domain and email done

--- script.js
+++ script.js
@@ -11,48 +11,63 @@ let addressMap = document.getElementById("address-map")
 //Function to search IP address
 const searchIpAddress = () => {
     ipAddressValue = searchInput.value; //ipAddressValue can be either ip address or domain or email address
-    fetch(`https://geo.ipify.org/api/v1?apiKey=at_9AmVz8EqulLVCSQpGAhA9JFPvBfC1&ipAddress=${ipAddressValue}`)
-    .then((res) => res.json())
+    const geoUrls = [
+        `https://geo.ipify.org/api/v1?apiKey=at_9AmVz8EqulLVCSQpGAhA9JFPvBfC1&ipAddress=${ipAddressValue}`,
+        `https://geo.ipify.org/api/v1?apiKey=at_9AmVz8EqulLVCSQpGAhA9JFPvBfC1&domain=${ipAddressValue}`,
+        `https://geo.ipify.org/api/v1?apiKey=at_9AmVz8EqulLVCSQpGAhA9JFPvBfC1&email=${ipAddressValue}`
+    ]
+
+    Promise.all(geoUrls.map(url => 
+        fetch(url)
+        .then((res) => res.json())
+        .catch((error) => console.log(error))
+    ))
     .then((data) => {
         console.log(data)
-        ipAddress.innerHTML = data.ip
-        locationDetails.innerHTML = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`
-        timezone.innerHTML = data.location.timezone
-        isp.innerHTML = data.isp
+        data.map((item) => {
+            console.log(item)
+            if (item.code == 422){
+                return null 
+            }else{
 
-        const lat = data.location.lat
-        const lng = data.location.lng
-        
-        // This checks if map is already initiated or not 
-        var container = L.DomUtil.get('mapItem');
-        if(container != null){
-            container._leaflet_id = null;
+            ipAddress.innerHTML = item.ip
+            locationDetails.innerHTML = `${item.location.city}, ${item.location.country} ${item.location.postalCode}`
+            timezone.innerHTML = item.location.timezone
+            isp.innerHTML = item.isp
+
+            const lat = item.location.lat
+            const lng = item.location.lng
+
+             // This checks if map is already initiated or not 
+            var container = L.DomUtil.get('mapItem');
+            if(container != null){
+                container._leaflet_id = null;
+            }
+
+            // Creating map options
+            let mapOptions = {
+                center: [lat, lng],
+                zoom: 13
+            }
+
+            // Creating a map object
+            let map = L.map('mapItem', mapOptions);
+
+            // Creating a layer object
+            let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+            // Adding layer to map
+            map.addLayer(layer);
+
+            // Creating a marker
+            let marker = L.marker([lat, lng]);
+
+            // Adding Marker to Map
+            marker.addTo(map);
         }
+        })
 
-        // Creating map options
-        let mapOptions = {
-            center: [lat, lng],
-            zoom: 13
-        }
-
-        // Creating a map object
-        let map = L.map('mapItem', mapOptions);
-
-        // Creating a layer object
-        let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-
-        // Adding layer to map
-        map.addLayer(layer);
-
-        // Creating a marker
-        let marker = L.marker([lat, lng]);
-
-        // Adding Marker to Map
-        marker.addTo(map);
-
-        
     })
-    .catch((error) => console.log(error))
 }
 
 searchIpAddress()
